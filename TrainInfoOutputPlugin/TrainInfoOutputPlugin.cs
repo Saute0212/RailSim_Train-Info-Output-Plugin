d1581392b7b70ce0589e1fb6507ecffeb8cd365d
Saute0212 <153172621+Saute0212@users.noreply.github.com>
2024-08-16 17:09:47 +0900

    機能追加：各種関数（Load, Dispose etc）追加

--- TrainInfoOutputPlugin/TrainInfoOutputPlugin.cs
+++ TrainInfoOutputPlugin/TrainInfoOutputPlugin.cs
@@ -11,6 +11,102 @@ namespace TrainInfoOutputPlugin
         private static Settings settingsEnv = null;
         private static Settings.OutputSetting settingsStatus;
 
+        //プラグイン読み込み時に実行
+        public void Load()
+        {
+
+        }
+
+        //プラグイン解放時に実行
+        public void Dispose()
+        {
+
+        }
+
+        //ATSプラグインのバージョンを返す
+        public int GetPluginVersion()
+        {
+            return settingsEnv.PluginVersion;
+        }
+
+        //車両読み込み時に実行
+        public void SetVehicleSpec()
+        {
+
+        }
+
+        //ゲーム開始時に実行
+        public void Initialize(int brake)
+        {
+
+        }
+
+        //毎フレーム実行
+        public void Elapse()
+        {
+
+        }
+
+        //主ハンドル操作時に実行
+        public void SetPower(int notch)
+        {
+
+        }
+
+        //ブレーキ操作時に実行
+        public void SetBrake(int notch)
+        {
+
+        }
+
+        //レバーサー操作時に実行
+        public void SetReverser(int pos)
+        {
+
+        }
+
+        //ATSキーが押されたときに実行
+        public void KeyDown(int atsKeyCode)
+        {
+
+        }
+
+        //ATSキーが離されたときに実行
+        public void KeyUp(int atsKeyCode)
+        {
+
+        }
+
+        //警笛操作時に実行
+        public void HornBlow(int hornType)
+        {
+
+        }
+
+        //ドアが開いたときに実行
+        public void DoorOpen()
+        {
+
+        }
+
+        //ドアが閉まったときに実行
+        public void DoorClose()
+        {
+
+        }
+
+        //閉塞の信号が変化したときに実行
+        public void SetSignal(int signal)
+        {
+
+        }
+
+        //地上子を超えたときに実行
+        public void SetBeaconData()
+        {
+
+        }
+
         //設定の読み込み
         private void LoadSettings()
         {
