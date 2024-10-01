865269293fc4be91f97fffcc96393abc1dbd789f
Saute0212 <153172621+Saute0212@users.noreply.github.com>
2024-10-01 14:15:34 +0900

    機能追加：DllExportのインストール実行ファイル(InstallDllExport.bat)

---
+++ InstallDllExport.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+set "url=https://github.com/3F/DllExport/releases/download/v1.7.4/DllExport.bat"
+set "output=DllExport.bat"
+
+echo Downloading file...
+curl -o "%output%" "%url%"
+
+if exist "%output%" (
+    echo Download complete. Executeing file...
+    call "%output%"
+) else (
+    echo Download failed.
+)
+
+endlocal
