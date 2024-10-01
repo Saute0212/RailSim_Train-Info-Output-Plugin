d5a9de1c5247aece5016b6d24a011e7e4d886abf
Saute0212 <153172621+Saute0212@users.noreply.github.com>
2024-10-01 16:32:45 +0900

    バグ修正：DllExportのインストール実行ファイル(InstallDllExport.bat)

--- InstallDllExport.bat
+++ InstallDllExport.bat
@@ -1,12 +1,12 @@
 @echo off
 setlocal
 
-set "url=https://github.com/3F/DllExport/releases/download/v1.7.4/DllExport.bat"
-set "output=DllExport.bat"
+set url="https://github.com/3F/DllExport/releases/download/v1.7.4/DllExport.bat"
+set output="DllExport.bat"
 
 echo Downloading file...
 
-powershell -command "Invoke-WebRequest -Uri '%url%' -OutFile '%output%'"
+curl -o "%output%" "%url%"
 
 if exist "%output%" (
     echo Download complete. Executeing file...
