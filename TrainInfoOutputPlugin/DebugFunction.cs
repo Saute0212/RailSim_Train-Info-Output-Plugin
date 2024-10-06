b015b512e544c9c9667a0b85670d932ff71edbaf
Saute0212 <153172621+Saute0212@users.noreply.github.com>
2024-10-06 18:37:15 +0900

    機能追加：デバッグ用コンソール

--- TrainInfoOutputPlugin/DebugFunction.cs
+++ TrainInfoOutputPlugin/DebugFunction.cs
@@ -1,11 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace TrainInfoOutputPlugin
 {
-    internal class DebugFunction
+    public class DebugFunction
     {
+        //コンソール用Dllをインポート
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool AllocConsole();
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool FreeConsole();
+
+        //コンソールの状態
+        private bool isConsoleOpen = false;
+
+        //コンソールの初期化
+        public void InitConsole()
+        {
+            ShowConsole();
+            DisplayString("The debug console has been launched.");
+        }
+
+        //コンソールを表示
+        public void ShowConsole()
+        {
+            if(!isConsoleOpen)
+            {
+                try
+                {
+                    AllocConsole();
+                    isConsoleOpen = true;
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        //文字列を表示
+        public void DisplayString(string str)
+        {
+            if(isConsoleOpen)
+            {
+                try
+                {
+                    Console.WriteLine(str);
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        //コンソールを閉じる
+        public void CloseConsole()
+        {
+            if (isConsoleOpen)
+            {
+                try
+                {
+                    FreeConsole();
+                    isConsoleOpen= false;
+                }
+                catch
+                {
+                }
+            }
+        }
     }
 }
