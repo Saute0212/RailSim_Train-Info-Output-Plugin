3e38b0d9f94852d3606b2d3cf6aab44fce5969ec
Saute0212 <153172621+Saute0212@users.noreply.github.com>
2024-11-24 11:21:14 +0900

    ファイル追加

---
+++ TrainInfoOutputPlugin/FalsePassagePreventionDevice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrainInfoOutputPlugin
+{
+    internal class FalsePassagePreventionDevice
+    {
+    }
+}
