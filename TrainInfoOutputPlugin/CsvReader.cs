00589c1603daeafc2ec5b1efc6fc016e4ac9b2d0
Saute0212 <153172621+Saute0212@users.noreply.github.com>
2024-12-13 01:41:27 +0900

    機能追加：csvファイルの読み込み

--- TrainInfoOutputPlugin/CsvReader.cs
+++ TrainInfoOutputPlugin/CsvReader.cs
@@ -2,10 +2,79 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace TrainInfoOutputPlugin
 {
-    internal class CsvReader
+    public class CsvReader
     {
+        //csvファイルの読み込み
+        public string[,] ReadCsv(string CsvPath, bool FillWithSharp)
+        {
+            var lines = File.ReadAllLines(CsvPath);
+
+            int rowCount = lines.Length;
+            int colCount = lines[0].Split(',').Length;
+
+            string[, ] result = new string[rowCount, colCount];
+
+            for(int i = 0; i < rowCount; i++)
+            {
+                string[] tmp1 = lines[i].Split(',');
+
+                for(int j = 0; j< tmp1.Length; j++)
+                {
+                    string tmp2 = FixCsvFormat(tmp1[j]);
+
+                    if(StartWithSharp(tmp2))
+                    {
+                        if (FillWithSharp)
+                        {
+                            for(int k = j; k < tmp1.Length; k++)
+                            {
+                                result[i, k] = "#";
+                            }
+                            break;
+                        }
+                        else
+                        {
+                            result[i, j] = "#";
+                        }
+                    }
+                    else
+                    {
+                        result[i, j] = tmp2;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        //ダブルクォーテーション・空白の除去
+        public string FixCsvFormat(string str)
+        {
+            if (str.StartsWith("\"") && str.EndsWith("\""))
+            {
+                str = str.Trim('"');
+            }
+
+            str = str.Replace(" ", "");
+
+            return str;
+        }
+
+        //コメントアウト(#)の探索
+        public bool StartWithSharp(string str)
+        {
+            bool result = false;
+
+            if (str.StartsWith("#"))
+            {
+                result = true;
+            }
+
+            return result;
+        }
     }
 }
