84435eb9bd17e45422cda9a44736a4b6ad658e1d
Saute0212 <153172621+Saute0212@users.noreply.github.com>
2024-12-14 19:24:55 +0900

    機能追加：後退検知

--- TrainInfoOutputPlugin/ReverseMotionDetection.cs
+++ TrainInfoOutputPlugin/ReverseMotionDetection.cs
@@ -5,7 +5,29 @@ using System.Text;
 
 namespace TrainInfoOutputPlugin
 {
-    internal class ReverseMotionDetection
+    //後退検知機能
+    public class ReverseMotionDetection
     {
+        public int RunRMD(float Speed, int ReverserPos, int BrakeNotch, int BrakeNotches)
+        {
+            int BrakeOutput = BrakeNotch;
+            
+            if(IsHighSpeed(Speed) && IsReverserInReverse(ReverserPos))
+            {
+                BrakeOutput = BrakeNotches;
+            }
+
+            return BrakeOutput;
+        }
+
+        private bool IsHighSpeed(float speed)
+        {
+            return speed >= 5.0;
+        }
+
+        private bool IsReverserInReverse(int RevPos)
+        {
+            return RevPos < 0;
+        }
     }
 }
